34897e5fe471f0b25f4e5caebecb3545fe18a2b0
aviadEshel <aviad@eshel.us>
2022-06-19 11:48:10 +0300

    add second print line

--- src/top_layer/Main/Main.java
+++ src/top_layer/Main/Main.java
@@ -3,5 +3,6 @@ package top_layer.Main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("second message");
     }
 }
