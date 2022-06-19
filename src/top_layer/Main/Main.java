6cfd757f9982edee39a1199aab73834a63d77e36
aviadEshel <aviad@eshel.us>
2022-06-19 13:03:08 +0300

    thirs print line added by me and me alone!!

--- src/top_layer/Main/Main.java
+++ src/top_layer/Main/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("second message");
+        System.out.println("third message");
     }
 }
