d949f9c408d3866551031fd6282f402badc8f915
aviadEshel <avaid@eshel.us>
2022-06-27 14:15:58 +0300

    work dammit

--- src/top_layer/Main/Main.java
+++ src/top_layer/Main/Main.java
@@ -1,5 +1,7 @@
 package top_layer.Main;
 
+import top_layer.Main.secondary.Functions;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!!!!");
@@ -7,5 +9,7 @@ public class Main {
         System.out.println("third message");
         System.out.println("tzipi is pretty");
         System.out.println("i love tzipiy");
+        Functions functions =new Functions();
+        functions.function();
     }
 }
---
+++ src/top_layer/Main/secondary/Functions.java
@@ -0,0 +1,10 @@
+package top_layer.Main.secondary;
+
+public  class Functions {
+    public Functions() {
+    }
+
+    public void function (){
+        System.out.println("hello merge");
+    }
+}
